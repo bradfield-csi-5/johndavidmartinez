8ed8073e7f8ddc27d9ef774d07bd4d8fbab03182
John Martinez <mail@johndavidmartinez.com>
2023-06-28 16:42:10 -0400

    rewrite plans

---
+++ prep/go/tutorial/chapter4/rewrite_xkcd.go
@@ -0,0 +1,7 @@
+// Rewrite with the following goals:
+//   - Maximize speed
+//   - More user friendliness
+//   - Testing & Benchmarking as design influence
+//   - Separate indexing from searching
+//   - Organize datastructures and interfaces better
+//
