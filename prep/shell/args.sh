d2647375c1a9b3837d10ac3a8101361e45bce296
John Martinez <mail@johndavidmartinez.com>
2023-08-18 22:28:12 -0400

    rm walrus

--- prep/shell/args.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "File name is "$0
-echo $3
-DATA=$5
-echo "A $DATA costs just $6."
-echo $#
--- prep/shell/pipe.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# print processors on system
-P_COUNT=$(cat /proc/cpuinfo | grep processor | wc -l)
-echo "Processor count: $P_COUNT"
